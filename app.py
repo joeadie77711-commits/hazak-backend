1e37a4ec3e9c379836857ca4f6bb08c17282fb4c
Joe Adie <you@example.com>
2025-11-12 08:46:00 +0800

    Tukar port ke 8080 untuk Render

--- app.py
+++ app.py
@@ -71,4 +71,6 @@ def get_users():
     return jsonify({"current_user": current_user, "users": users})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
